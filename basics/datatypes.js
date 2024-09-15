289e181e4701e5bb9a7f2d6f7ce692c690f4aa2d
usman034041 <muhammadusmanirshad81@gmail.com>
2024-09-15 10:47:04 +0000

    container config done

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,6 +1,39 @@
 "use strict";
-let sccor="16"
-console.log(typeof (sccor));
-let valueInNulber=Number(sccor);
-console.log(typeof valueInNulber);
-console.log(valueInNulber);
+// let sccor="16"
+// console.log(typeof (sccor));
+// let valueInNulber=Number(sccor);
+// console.log(typeof valueInNulber);
+// console.log(valueInNulber);
+// let isloggedIn=1;
+// let booleanconversion=Boolean(isloggedIn);
+// console.log(booleanconversion);
+// let firstNumber="usman irshad"
+// let secondNumber=firstNumber
+//  secondNumber="ali "
+//  console.log(firstNumber);
+//  console.log(secondNumber);
+//  let firstobject={
+//     email:"usmanirshad81@gmal.com",
+//     num1:"03404184810",
+//  }
+//  let secondobject=firstobject
+//  secondobject.email="usman456"
+//  console.log(firstobject.email);
+ 
+//  console.log(secondobject.email);
+//  const name="usman"
+//  const fname="irshad"
+//  const pnumber="0758981377"
+//  console.log(`my name is ${name}and my father name is ${fname} and pnumber is ${pnumber}`);
+  const gamename=new String("abubakar" )
+//  console.log(gamename.length,gamename.__proto,gamename[0],gamename.toUpperCase(),gamename.toLocaleLowerCase(),gamename.toLocaleUpperCase(),gamename.trimStart());
+     const othergame=gamename.substring(0,4)
+console.log(othergame)
+const angame=gamename.slice(-4,8)
+ console.log(angame)
+ 
+ 
+
+
+ 
+ 
