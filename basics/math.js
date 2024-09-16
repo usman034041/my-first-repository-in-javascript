b2810899e40f2c004d0e099e510e0551495e7ca3
usman034041 <muhammadusmanirshad81@gmail.com>
2024-09-16 07:16:55 +0000

    container config done

--- basics/math.js
+++ basics/math.js
@@ -1,9 +1,25 @@
 "use strict"
-const n1=1000000000000
-const n2=new Number(200.546)
-console.log(n1);
-console.log(n2);
-console.log(n2.toPrecision(4),n2.toFixed(2),n2.toString().length,);
-console.log(n1.toLocaleString('en-PK'));
+// const n1=1000000000000
+// const n2=new Number(200.546)
+// console.log(n1);
+// console.log(n2);
+// console.log(n2.toPrecision(4),n2.toFixed(2),n2.toString().length,);
+// console.log(n1.toLocaleString('en-PK'));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(25));
+const min =10
+const max=20
+console.log(Math.random());
+
+console.log(Math.random()*(max - min + 1) + min);
+
+
+
+
+
+
 
 
