d9042cd04dd7bd3f85a9d8ec9f6e05e6b99df026
usman034041 <muhammadusmanirshad81@gmail.com>
2024-09-16 06:32:27 +0000

    container config done

---
+++ basics/math.js
@@ -0,0 +1,9 @@
+"use strict"
+const n1=1000000000000
+const n2=new Number(200.546)
+console.log(n1);
+console.log(n2);
+console.log(n2.toPrecision(4),n2.toFixed(2),n2.toString().length,);
+console.log(n1.toLocaleString('en-PK'));
+
+
