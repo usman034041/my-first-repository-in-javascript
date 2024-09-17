27e8684a2c3a54ca7815a4943ea5fa879c6a1872
usman034041 <muhammadusmanirshad81@gmail.com>
2024-09-17 08:47:11 +0000

    container config done

--- basics/object.js
+++ basics/object.js
@@ -1,5 +1,5 @@
 "use strict"
-const first_symbol=Symbol("key1")
+const first_symbol= Symbol("key1")
 const first_object={
     name: "usman",
     "last_name" : "irshad",
@@ -9,5 +9,24 @@ const first_object={
 
 
 }
-console.log(first_object[first_object]);
+// first_object.email = "thisismyemail@"
+// console.log(first_object[]);
+
+// console.log(typeof first_object[first_symbol]);
+// console.log(first_object);
+// Object.freeze(first_object);
+// first_object.email="udkljfjb@"
+// console.log(first_object)
+first_object.greeting = function(){
+console.log("hello this is usman");
+
+}
+console.log(first_object.greeting());
+first_object.greetingtwo= function(){
+    console.log(`hello this is ${this.name} father_name is ${this.last_name}`);
+    
+}
+console.log(first_object.greetingtwo());
+
+
 
