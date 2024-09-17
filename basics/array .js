cb9c7d4f5df09fa4b21e1fe091f179f4914fe8b6
usman034041 <muhammadusmanirshad81@gmail.com>
2024-09-17 05:43:52 +0000

    container config done

---
+++ basics/array .js
@@ -0,0 +1,30 @@
+"use string"
+const myarray= [1,2,3,4,5,6]
+// console.log(myarray);
+
+// console.log(myarray.push());
+// console.log(myarray);
+// const afpop=myarray.pop();
+// console.log(afpop);
+// console.log(myarray);
+const arra3=new Array("usman" ,"irshad" ,"awan")
+const arr4=
+// const myarray2=myarray.push(10)
+//  console.log(myarray2);
+
+// console.log(myarray.unshift(5));
+
+// console.log(myarray.length);
+// console.log(myarray.shift());
+// console.log(myarray);
+// const striarray=myarray.join()
+// console.log(striarray);
+// console.log(striarray.includes(10));
+// // console.log(striarray.indexOf(4));
+
+
+
+
+
+
+
