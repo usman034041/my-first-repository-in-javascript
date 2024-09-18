91eefbe055b748896502326a2ee57ef3517537fc
usman034041 <muhammadusmanirshad81@gmail.com>
2024-09-18 08:42:07 +0000

    container config done

---
+++ basics/02_object.js
@@ -0,0 +1,37 @@
+"user strict"
+// const firstcontructor=new Object()
+// console.log(firstcontructor);
+// firstcontructor.email="usman@yahoo.com";
+// firstcontructor.name="usman";
+// console.log(firstcontructor);
+// const firstcontructor=new Object(name ="usman",lastname="irshad",age=12)
+// console.log(firstcontructor.age);
+// const firstcontructor={
+//     name :"usman",
+//     lastname:{
+//         firstname:"ali",
+//         laname : "arshaad",
+//         email : {
+//             useremail:"usmanirshad@",
+//             username:"ayisha"
+//         }
+//     }
+// }
+// console.log(firstcontructor.lastname.email.username);
+const obj1 ={a :"1" , b : "2" ,c :"3"}
+const obj2 ={d :"4" , e : "5" ,f :"6"}
+// const obj3={obj1 , obj2}
+// const obj3 = Object.assign({} , obj1 , obj2)
+// const obj3 = {...obj1 ,  ...obj2} 
+// console.log(obj3);
+// console.log(Object.keys( obj2));
+// console.log(Object.values( obj2));
+// console.log(obj2.hasOwnProperty('g'));
+// const name=obj1.a
+
+const {a :first_number}= obj1
+console.log(first_number);
+
+
+
+
