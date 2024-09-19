73fffd9fa63ac22ecff19ff6831fcca182609bc3
usman034041 <muhammadusmanirshad81@gmail.com>
2024-09-19 11:26:54 +0000

    container config done

---
+++ basics/04control.js
@@ -0,0 +1,11 @@
+"use strict"
+// const userarray=[]
+// if (userarray.length==1) {
+//     console.log('hello array value is always true');
+    
+    
+// }
+const first_object={}
+if (condition) {
+    
+}
