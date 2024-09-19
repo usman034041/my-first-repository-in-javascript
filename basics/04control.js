70149677b400afd216ddaabcb80cc5eb81c22bfe
usman034041 <muhammadusmanirshad81@gmail.com>
2024-09-19 11:55:59 +0000

    container config done

--- basics/04control.js
+++ basics/04control.js
@@ -5,7 +5,15 @@
     
     
 // }
-const first_object={}
-if (condition) {
+// const first_object={}
+// if (Object.keys(first_object).length==0) {
     
-}
+//     console.log("this is my first object empty");
+// }
+// nullish coadescing opertator ??
+// let var1 ;
+// var1=undefined ?? null ?? 10
+// console.log(var1);
+const price=100
+price<=80 ? console.log("price is less than 80"):console.log("price more then 80");
+;
